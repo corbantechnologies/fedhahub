5121e0fed2d6b3d3be9d75367d9af23e648951de
Dalienst <oduordalienst@gmail.com>
2026-03-05 11:53:05 +0300

    update news

--- services/news.tsx
+++ services/news.tsx
@@ -7,12 +7,14 @@ import { apiActions } from "@/tools/axios";
 export interface News {
     id: string;
     title: string;
+    image: string;
     slug: string;
     category: string;
     brief: string;
     body: string; // it is in markdown format
     read_time_minutes: number;
     source_url: string | null;
+    is_featured: boolean;
     is_published: boolean;
     reference: string;
     created_at: string;
